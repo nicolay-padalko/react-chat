54ec42ca14d9cbe8f6440b469427b6ec7fc1d326
Nicolay <nicolay.padalko@gmail.com>
2019-05-17 20:29:29 -0300

    edit2,

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './components/App';
 import Login from './components/Auth/Login';
-import Register from './components/Auth/Register';
+import Registro from './components/Auth/Registro';
+
+import 'semantic-ui-css/semantic.min.css';
+
 import registerServiceWorker from './registerServiceWorker';
 
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
@@ -13,10 +15,10 @@ const Root = () => (
     <Switch>
       <Route exact path="/" component={App} />
       <Route path="/login" component={Login} />
-      <Route path="/register" component={Register} />
+      <Route path="/registro" component={Registro} />
     </Switch>
   </Router>
 );
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<Root />, document.getElementById('root'));
 registerServiceWorker();
