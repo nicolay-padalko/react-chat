41d8ff18e048e39d6364c58de638881ffa76fd88
Nicolay <nicolay.padalko@gmail.com>
2019-05-17 10:01:33 -0300

    edit1

---
+++ src/components/Auth/Register.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+class Register extends React.Component {
+  render() {
+    return (
+      <div>Registro</div>
+    )
+  }
+}
+
+export default Register;
---
+++ src/firebase.js
@@ -0,0 +1,19 @@
+
+import firebase from 'firabse/app';
+import "'firebase/auth";
+import "firebase/database";
+import "firebase/storage";
+
+var firebaseConfig = {
+  apiKey: "AIzaSyBPCdeiGfJnAheNeqT8z2VjLOIZionljeQ",
+  authDomain: "react-chat-aps-5-sem.firebaseapp.com",
+  databaseURL: "https://react-chat-aps-5-sem.firebaseio.com",
+  projectId: "react-chat-aps-5-sem",
+  storageBucket: "react-chat-aps-5-sem.appspot.com",
+  messagingSenderId: "340390402374",
+  appId: "1:340390402374:web:0f7e5890ccb63f9f"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+export default firebase;
